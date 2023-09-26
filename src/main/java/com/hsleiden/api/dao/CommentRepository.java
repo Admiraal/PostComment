33a7f9df1cf5cd63ddc80e30fdac3691fd767124
Admiraal <florisadmiraal@gmail.com>
2023-09-26 10:46:19 +0200

    Added repository annotation to CommentRepository

--- src/main/java/com/hsleiden/api/dao/CommentRepository.java
+++ src/main/java/com/hsleiden/api/dao/CommentRepository.java
@@ -2,6 +2,8 @@ package com.hsleiden.api.dao;
 
 import com.hsleiden.api.model.Comment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 }
